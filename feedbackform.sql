7b758171197e6756b92d318b4198ad3662896ad5
codebuff95 <tanmaygarg95@live.com>
2015-12-19 18:16:38 +0530

    Created tables department, faculty, subject and added dummy data
    Created tables.
    Added dummy faculties, departments to check the functioning of
    addsubject.php page. NOTE: do not manually add subjects into the
    database, do it through the addsubject.php page

--- feedbackform.sql
+++ feedbackform.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 19, 2015 at 08:45 AM
+-- Generation Time: Dec 19, 2015 at 01:44 PM
 -- Server version: 5.6.15-log
 -- PHP Version: 5.5.8
 
@@ -39,6 +39,66 @@ CREATE TABLE IF NOT EXISTS `admin` (
 INSERT INTO `admin` (`username`, `password`, `name`) VALUES
 ('tanmay', '123456', 'Tanmay Garg'),
 ('anuparna', '123456', 'Anuparna Deb');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE IF NOT EXISTS `department` (
+  `hodno` int(5) NOT NULL,
+  `dno` int(5) NOT NULL,
+  `dname` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`hodno`, `dno`, `dname`) VALUES
+(1, 1, 'Computer Science'),
+(3, 2, 'Information Technolo');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faculty`
+--
+
+CREATE TABLE IF NOT EXISTS `faculty` (
+  `fname` varchar(30) NOT NULL,
+  `fno` int(5) NOT NULL,
+  `dno` int(5) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `faculty`
+--
+
+INSERT INTO `faculty` (`fname`, `fno`, `dno`) VALUES
+('Dr. Vinita Singh', 1, 1),
+('Dr. Anupam Sharma', 2, 1),
+('Mr. Manoj Kumar', 3, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject`
+--
+
+CREATE TABLE IF NOT EXISTS `subject` (
+  `scode` varchar(20) NOT NULL,
+  `sname` varchar(20) NOT NULL,
+  `dno` int(5) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `subject`
+--
+
+INSERT INTO `subject` (`scode`, `sname`, `dno`) VALUES
+('CS-101', 'Operating Systems', 1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
